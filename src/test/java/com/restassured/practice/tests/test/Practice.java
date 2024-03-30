3a39d93ccfa653bac8a61143e2362fdae85c2d10
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-03-30 21:15:38 +0530

    introducing JsonPath to access response key-values

--- src/test/java/com/restassured/practice/tests/test/Practice.java
+++ src/test/java/com/restassured/practice/tests/test/Practice.java
@@ -2,19 +2,28 @@ package com.restassured.practice.tests.test;
 
 import com.restassured.practice.tests.data.Payload;
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
 public class Practice {
     public static void main(String[] args) {
         RestAssured.baseURI = "https://rahulshettyacademy.com";
+        String response =
         given().log().all().queryParam("key", "qaclick123")
                 .header("Content-Type", "application/json")
                 .body(Payload.addPlace())
         .when().post("/maps/api/place/add/json")
-        .then().log().all().assertThat().statusCode(200)
+        .then().assertThat().statusCode(200)
                 .body("scope", equalTo("APP"))
-                .header("server", "Apache/2.4.52 (Ubuntu)");
-    }
+                .header("server", "Apache/2.4.52 (Ubuntu)")
+                .extract().response().asString();
 
+        System.out.println(response);
+
+        JsonPath jsonPath = new JsonPath(response);
+        String placeID = jsonPath.getString("place_id");
+        System.out.println(placeID);
+    }
 }
