03837594f19f710d54b5a0b958d41dc7b20666b1
SayanDasguptaSDG <sayan.dg1995@gmail.com>
2024-04-07 18:57:43 +0530

    parameterize tests with TestNG annotation @DataProvider

--- src/test/java/com/restassured/practice/tests/test/LibraryTest.java
+++ src/test/java/com/restassured/practice/tests/test/LibraryTest.java
@@ -4,6 +4,7 @@ import com.restassured.practice.tests.data.Payload;
 import com.restassured.practice.tests.helper.Helper;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -11,17 +12,22 @@ import static io.restassured.RestAssured.given;
 public class LibraryTest {
     JsonPath jsonPath;
 
-    @Test
-    public void addBook() {
+    @Test(dataProvider = "BooksData")
+    public void addBook(String isbn, String aisle) {
         RestAssured.baseURI = "http://216.10.245.166";
         String response =
         given().header("Content-Type", "application/json")
-                .body(Payload.AddBook("zgtsfd", "1234"))
+                .body(Payload.AddBook(isbn, aisle))
         .when().post("/Library/Addbook.php")
         .then().assertThat().statusCode(200).extract().response().asString();
 
         jsonPath = Helper.rawToJson(response);
         String id = jsonPath.get("ID");
         System.out.println(id);
+    }
+
+    @DataProvider(name="BooksData")
+    public Object[] [] getData() {
+        return new Object [][] {{"ads", "1221"}, {"fgtgh", "535"}, {"sdgvbdsfhbr", "980"}};
     }
 }
